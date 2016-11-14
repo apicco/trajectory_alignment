16f2afbea942422b21035e9dc1daa01754479d8d
Andrea Picco <andreapicco@Andrea-2.local>
2016-11-14 19:14:52 +0100

    Editing the file to be identical to the wiki

--- example/trajectory_average_example.py
+++ example/trajectory_average_example.py
@@ -25,24 +25,32 @@ best_average , worst_average = average_trajectories( trajectory_list , max_frame
 
 plt.figure()
 plt.subplot(221)
-plt.plot( best_average.coord()[ 0 ] , best_average.coord()[ 1 ] , 'k-' , label = best_average.annotations( 'protein' ) + ' average trajectory')
-plt.plot( best_average.coord()[ 0 ] , best_average.coord()[ 1 ] * 0 , 'r--' , label = 'invagination axis')
+plt.plot( best_average.coord()[ 0 ] , best_average.coord()[ 1 ] ,\
+		'k-' , label = best_average.annotations( 'protein' ) + ' average trajectory')
+plt.plot( best_average.coord()[ 0 ] , best_average.coord()[ 1 ] * 0 ,\
+		'r--' , label = 'invagination axis')
 plt.xlabel( 'Inward movement (' + best_average.annotations( 'coord_unit' ) + ')' )
 plt.ylabel( best_average.annotations( 'coord_unit' ) )
 plt.legend( loc = 'best' )
 
 plt.subplot(222)
-plt.plot( best_average.t() , best_average.coord()[ 0 ] , 'k-' , label = best_average.annotations( 'protein' ) + ' inward movement')
-plt.plot( best_average.t() , best_average.coord()[ 0 ] + best_average.coord_err()[ 0 ], '--' , color = '0.75' , label = 'Std. of the aligned\ntrajectories')
-plt.plot( best_average.t() , best_average.coord()[ 0 ] - best_average.coord_err()[ 0 ], '--' , color = '0.75' )
+plt.plot( best_average.t() , best_average.coord()[ 0 ] ,\
+		'k-' , label = best_average.annotations( 'protein' ) + ' inward movement')
+plt.plot( best_average.t() , best_average.coord()[ 0 ] + best_average.coord_err()[ 0 ],\
+		'--' , color = '0.75' , label = 'Std. of the aligned\ntrajectories')
+plt.plot( best_average.t() , best_average.coord()[ 0 ] - best_average.coord_err()[ 0 ],\
+		'--' , color = '0.75' )
 plt.xlabel( 'Time (' + best_average.annotations( 't_unit' ) + ')' )
 plt.ylabel( 'Inward movement (' + best_average.annotations( 'coord_unit' ) + ')' )
 plt.legend( loc = 'best' )
 
 plt.subplot(224)
-plt.plot( best_average.t() , best_average.f() , 'k-' , label = best_average.annotations( 'protein' ) + ' normalized\nfluorescence intensity')
-plt.plot( best_average.t() , best_average.f() + best_average.f_err(), '--' , color = '0.75' , label = 'Std. of the fluorescence intensity')
-plt.plot( best_average.t() , best_average.f() - best_average.f_err(), '--' , color = '0.75' )
+plt.plot( best_average.t() , best_average.f() , 'k-' ,\
+		label = best_average.annotations( 'protein' ) + ' normalized\nfluorescence intensity')
+plt.plot( best_average.t() , best_average.f() + best_average.f_err(),\
+		'--' , color = '0.75' , label = 'Std. of the fluorescence intensity')
+plt.plot( best_average.t() , best_average.f() - best_average.f_err(),\
+		'--' , color = '0.75' )
 plt.xlabel( 'Time (' + best_average.annotations( 't_unit' ) + ')' )
 plt.legend( loc = 'best' )
 
