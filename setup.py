3e6f0ed6a62bcedfacfe328efc6023ec24f35b8e
Andrea Picco <andreapicco@Andrea-2.local>
2016-11-15 17:43:51 +0100

    rename the folders, correct the examples and the setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup( name = 'trajectory_alignment' ,
+setup( name = 'trajectory alignment' ,
 		version = '0.1' ,
 		description = 'Utilities to Align and Average Trajectories' ,
 		author = 'Andrea Picco',
 		author_email = 'andrea.picco@unige.ch',
 		url = 'http://apicco.github.io/trajectory_alignment/',
-		packages = [ 'trajaverage' ] ,
+		modules = [ 'trajalign.traj' , 'trajalign.average' ] ,
 		)
