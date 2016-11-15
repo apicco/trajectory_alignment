f90d2ba364b933ae566d0828ca70ebb571debc72
Andrea Picco <andreapicco@Andrea-2.local>
2016-11-15 17:00:34 +0100

    Add the setup.py for installations

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup( name = 'trajectory_alignment' ,
+		version = '0.1' ,
+		description = 'Utilities to Align and Average Trajectories' ,
+		author = 'Andrea Picco',
+		author_email = 'andrea.picco@unige.ch',
+		url = 'http://apicco.github.io/trajectory_alignment/',
+		packages = [ 'format' , 'average' ] ,
+		)
