da94ccc589926e05c817f8c9b520f2803868ebfe
Andrea Picco <andreapicco@Andrea-3.local>
2017-04-12 11:14:44 +0200

    version 1.0, with clearer average.py, better norm_f() and better README

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup( name = 'trajectory alignment' ,
-		version = '0.9' ,
+		version = '1.0' ,
 		description = 'Utilities to Align and Average Trajectories' ,
 		author = 'Andrea Picco',
 		author_email = 'andrea.picco@unige.ch',
