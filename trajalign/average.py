d490d1ee57c99a94bc22ade85c1a44fd9d38932d
Andrea Picco <andreapicco@Andrea-3.local>
2018-11-27 16:43:53 +0100

    update version to 1.71

--- trajalign/average.py
+++ trajalign/average.py
@@ -16,7 +16,7 @@ import warnings as wr
 
 from sklearn import linear_model
 
-def header( version = 1.7 , year = 2018 , printit = True ) :
+def header( version = 1.71 , year = 2018 , printit = True ) :
 
 	if printit :
 
