db5cf5dfd8274244ad5fca0b3bccc1f437c5370c
Andrea Picco <andreapicco@Andrea-3.local>
2019-04-01 16:30:49 +0200

    remove \t from version header

--- trajalign/average.py
+++ trajalign/average.py
@@ -21,7 +21,7 @@ def header( version = 1.82 , year = 2018 , printit = True ) :
 	if printit :
 
 		print('|-----------------------------------------------------|')
-		print('| Trajalign version ' + str( version ) +'\t Copyright ' + str( year ) + ' Andrea Picco. |')
+		print('| Trajalign version ' + str( version ) +' Copyright ' + str( year ) + ' Andrea Picco. |')
 		print('|   Url: www.apicco.github.io/trajectory_alignment/   |')
 		print('|-----------------------------------------------------|')
 	
