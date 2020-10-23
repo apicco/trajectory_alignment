837d457c3dec6306cb21076e6e9014353e455cb5
apicco <andrea.picco@unige.ch>
2020-10-23 10:37:11 +0200

    modify variable name in unified_start and unified_end for consitency. the variable is a trajectory t, not self

--- trajalign/average.py
+++ trajalign/average.py
@@ -175,21 +175,21 @@ def nanMAD( x , axis = None , k = 1.4826):
 	MAD = np.nanmedian( np.absolute( x - np.nanmedian( x , axis ) ) , axis )
 	return( k * MAD )
 	
-def unified_start( self ) :
+def unified_start( t ) :
 
 	try :
 
-		return float( self.annotations()[ 'mean_starts' ] ) - 1.96 * float( self.annotations()[ 'std_starts' ] ) / np.sqrt( float( self.annotations()[ 'n_starts' ] ) )
+		return float( t.annotations()[ 'mean_starts' ] ) - 1.96 * float( t.annotations()[ 'std_starts' ] ) / np.sqrt( float( t.annotations()[ 'n_starts' ] ) )
 		
 	except :
 		
 		print( 'Error: one or more of the annotations mean_starts, std_starts, and n_starts is/are missiong' )
 
-def unified_end( self ) :
+def unified_end( t ) :
 
 	try :
 
-		return float( self.annotations()[ 'mean_ends' ] ) + 1.96 * float( self.annotations()[ 'std_ends' ] ) / np.sqrt( float( self.annotations()[ 'n_ends' ] ) )
+		return float( t.annotations()[ 'mean_ends' ] ) + 1.96 * float( t.annotations()[ 'std_ends' ] ) / np.sqrt( float( t.annotations()[ 'n_ends' ] ) )
 		
 	except :
 		
