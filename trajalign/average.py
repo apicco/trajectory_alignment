064febb1a690ae3db2c42196bc7f220697dd83dd
apicco <andrea.picco@unige.ch>
2023-02-20 14:23:33 +0100

    add unified_start and unified_end to average

--- trajalign/average.py
+++ trajalign/average.py
@@ -876,3 +876,24 @@ def average_trajectories( trajectory_list , output_file = 'average' , median = F
 
     return( average_trajectory[ best_average ] , average_trajectory[ worst_average ] , aligned_trajectories[ best_average ] )
 
+def unified_start( t ) :
+
+    try :
+
+        return float( t.annotations()[ 'mean_starts' ] ) - 1.96 * float( t.annotations()[ 'std_starts' ] ) / np.sqrt( float( t.annotations()[ 'n_starts' ] ) )
+
+    except :
+
+        print( 'Error: one or more of the annotations mean_starts, std_starts, and n_starts is/are missiong' )
+
+def unified_end( t ) :
+
+    try :
+
+        return float( t.annotations()[ 'mean_ends' ] ) + 1.96 * float( t.annotations()[ 'std_ends' ] ) / np.sqrt( float( t.annotations()[ 'n_ends' ] ) )
+
+    except :
+
+        print( 'Error: one or more of the annotations mean_ends, std_ends, and n_ends is/are missiong' )
+
+
