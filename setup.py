7c3a9900d8bb100b36c2fb5e873c273454e286cd
apicco <andrea.picco@unige.ch>
2023-03-06 10:40:51 +0100

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup( name = 'trajectory alignment' ,
-		version = '1.9' ,
+		version = '1.91' ,
 		description = 'Utilities to Align and Average Trajectories' ,
 		author = 'Andrea Picco',
 		author_email = 'andrea.picco@unige.ch',
