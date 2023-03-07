b8d865ad88f7a34bec342795049bb256c64ca355
apicco <andrea.picco@unige.ch>
2023-03-07 17:19:41 +0100

    edit literal and update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup( name = 'trajectory alignment' ,
-		version = '1.93' ,
+		version = '1.94' ,
 		description = 'Utilities to Align and Average Trajectories' ,
 		author = 'Andrea Picco',
 		author_email = 'andrea.picco@unige.ch',
--- trajalign/average.py
+++ trajalign/average.py
@@ -876,11 +876,11 @@ def average_trajectories( trajectory_list , output_file = 'average' , median = F
 
     return( average_trajectory[ best_average ] , average_trajectory[ worst_average ] , aligned_trajectories[ best_average ] )
 
-def unified_start( t , 95CI = True ) :
+def unified_start( t , add_CI = True ) :
 
     try :
 
-        if 95CI : 
+        if add_CI : 
             return float( t.annotations()[ 'mean_starts' ] ) - 1.96 * float( t.annotations()[ 'std_starts' ] ) / np.sqrt( float( t.annotations()[ 'n_starts' ] ) )
         else : 
             return float( t.annotations()[ 'mean_starts' ] )
@@ -889,11 +889,11 @@ def unified_start( t , 95CI = True ) :
 
         print( 'Error: one or more of the annotations mean_starts, std_starts, and n_starts is/are missiong' )
 
-def unified_end( t , 95CI = True ) :
+def unified_end( t , add_CI = True ) :
 
     try :
 
-        if 95CI : 
+        if add_CI : 
             return float( t.annotations()[ 'mean_ends' ] ) + 1.96 * float( t.annotations()[ 'std_ends' ] ) / np.sqrt( float( t.annotations()[ 'n_ends' ] ) )
         else : 
             return float( t.annotations()[ 'mean_ends' ] )
